6fa210d2507dddc7680b3578727aee117af16f4b
elias adam <eliasadam@eliass-MBP-2.broadband>
2023-08-18 01:41:50 +0100

    created next13 app and installed shadcn

---
+++ ecomerce-admin-board/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p>Welcome Admin Dashboard</p>
+      <Button>Click Me</Button>
+    </div>
+    
+  )
+}
